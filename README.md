ad62a860f154759817870ae97ba6233fdc55f472
excelasaph <e.asaph@alustudent.com>
2023-10-15 11:16:26 -0700

    changing every file

--- README.md
+++ README.md
@@ -1,2 +1,93 @@
 # ALU_HTML-CSS-Contest_Group-11
-This is a group project
+
+Our team was tasked with developing and designing a website showcasing the basic skills we've learned in HTML and CSS including making the site responsive and we were not permitted to use a Content Management System or front-end framework/libraries to develop our website. Enjoy your read!
+
+# Nevents - Alumni Connect
+
+Nevents is a web application designed to connect alumni with exciting events and opportunities within the African Leadership University (ALU) community. This README provides an overview of the project, its features, setup instructions, and how to use it.
+
+![Nevents Landing Page](/img/screenshot.png)
+
+## Table of Contents
+
+- [Features](#features)
+- [Technologies](#technologies)
+- [Getting Started](#getting-started)
+  - [Installation](#installation)
+  - [Usage](#usage)
+- [Project Structure](#project-structure)
+- [Contributing](#contributing)
+- [License](#license)
+- [Acknowledgments](#acknowledgments)
+- [Contact](#contact)
+
+## Features
+
+- **Tab Navigation**: Navigating between different sections of the website using tabs.
+- **Interactive Tabs**: Tabs respond to user clicks and change the content accordingly.
+- **Form Label Animations**: Animations for form input labels for a better user experience.
+- **Responsive Design**: The website is responsive and adapts to various screen sizes.
+
+## Technologies
+
+![HTML5 Badge](https://img.shields.io/badge/HTML-239120?style=for-the-badge&logo=html5&logoColor=white) ![CSS3 Badge](https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white) ![JavaScript Badge](https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black)
+
+## Getting Started
+
+These instructions will help you set up and run the project locally on your machine.
+
+### Installation
+
+```bash
+# Clone the repository
+git clone https://github.com/maxprodigy/ALU_HTML-CSS-Contest_Group-11.git
+
+# Change directory to the project folder (if any)
+cd nevents-alumni-connect
+
+# Install dependencies (if any)
+npm install
+```
+
+### Usage
+
+- Open the `index.html` file in your web browser to run the website locally.
+
+## Project Structure
+
+The project structure is organised as follows:
+
+- `index.html`: The main HTML file for the website.
+- `style.css`: The CSS file containing styling rules.
+- `script.js`: The JavaScript file for handling interactions.
+- `img/`: Directory containing images used in the website.
+
+## Contributing
+
+Please read [CONTRIBUTING.md](https://contributing.md/) for details on our code of conduct, and the process for submitting pull requests to us. When done, follow these guidelines:
+
+1. Fork the repository.
+
+2. Create a new branch for your feature or bug fix.
+
+3. Make your changes and commit them with descriptive messages.
+
+4. Submit a pull request to the `master` branch.
+
+## License
+
+This project will be licensed under the MIT License - see the [LICENSE](LICENSE) file for details in future :).
+
+## Acknowledgments
+
+Below are the list of resources we used in our site.
+
+- [ALU Website](https://www.alueducation.com/)
+- [ALU Events Calendar](https://www.alueducation.com/events-calendar/)
+- [Project Rubric Document](./img/front-end_web_assignment_rubric.pdf)
+- [Canva](https://www.canva.com) for the Logo(s)
+- [Font Awesome](https://fontawesome.com/) for the Tab Icons
+
+## Contact
+
+If you have any questions, feedback, or collaboration requests, please feel free to reach out to us at [e.adewusi@alustudent.com](mailto:e.adewusi@alustudent.com).
--- script.js
+++ script.js
@@ -1,40 +1,42 @@
 const tabItems = document.querySelectorAll('.tab-item');
 const tabContentItems = document.querySelectorAll('.tab-content-item');
-const navItems = document.querySelectorAll('.nav-item')
-const eventClick = document.querySelector('.event-click')
-const contactClick = document.querySelector('.contact-click')
-const aboutClick = document.querySelector('.about-click')
-const eventRedirect = document.querySelector('.event-redirect')
-const contactRedirect = document.querySelector('.contact-redirect')
-const aboutRedirect = document.querySelector('.about-redirect')
-const eventResponse = document.querySelector('.event-response')
-const eventRequest = document.querySelector('.event-request')
-const contactResponse = document.querySelector('.contact-response')
-const contactRequest = document.querySelector('.contact-request')
-const aboutResponse = document.querySelector('.about-response')
-const aboutRequest = document.querySelector('.about-request')
-const eventFooterClick = document.querySelector('.event-footer-click')
-const contactFooterClick = document.querySelector('.contact-footer-click')
-const aboutFooterClick = document.querySelector('.about-footer-click')
-
-
+const navItems = document.querySelectorAll('.nav-item');
+const eventClick = document.querySelector('.event-click');
+const contactClick = document.querySelector('.contact-click');
+const aboutClick = document.querySelector('.about-click');
+const eventRedirect = document.querySelector('.event-redirect');
+const contactRedirect = document.querySelector('.contact-redirect');
+const aboutRedirect = document.querySelector('.about-redirect');
+const eventResponse = document.querySelector('.event-response');
+const eventRequest = document.querySelector('.event-request');
+const contactResponse = document.querySelector('.contact-response');
+const contactRequest = document.querySelector('.contact-request');
+const aboutResponse = document.querySelector('.about-response');
+const aboutRequest = document.querySelector('.about-request');
+const eventFooterClick = document.querySelector('.event-footer-click');
+const contactFooterClick = document.querySelector('.contact-footer-click');
+const aboutFooterClick = document.querySelector('.about-footer-click');
+const formContainer = document.querySelector('.form-container');
+const formBtn = document.querySelector('.form-btn');
+const fullNameInput = document.querySelector('.fullname-input');
+const emailInput = document.querySelector('.email-input');
 
 function selectNav(e) {
     removeNavBorder()
     this.classList.add('nav-border')
-}
+};
 
 function removeNavBorder() {
     navItems.forEach(item => item.classList.remove('nav-border'))
-}
+};
 
 function removeBorder() {
     tabItems.forEach(item => item.classList.remove('tab-border'))
-}
+};
 
 function removeShow() {
     tabContentItems.forEach(item => item.classList.remove('show'));
-}
+};
 
 function selectItem(e) {
     removeBorder()
@@ -45,79 +47,95 @@ function selectItem(e) {
     const tabContentItem = document.querySelector(`#${this.id}-content`)
     // Add show class
     tabContentItem.classList.add('show')
-}
+};
+
+function renderForm() {
+    if (fullNameInput.value && emailInput.value) {
+        formContainer.innerHTML = `
+        <h1>Thanks For Registering, ${fullNameInput.value}</h1>
+        <p class="text" style="text-align: center;">Don't forget to check your <a href="https://calendar.google.com/" target="_blank">calender</a> for future events </p>`
+    } else (
+        noText()
+    )
+};
+
+function noText() {
+    // render nothing
+};
 
 // Listen for tab click
-tabItems.forEach(Item => Item.addEventListener("click", selectItem))
+tabItems.forEach(Item => Item.addEventListener("click", selectItem));
 
-navItems.forEach(Item => Item.addEventListener("click", selectNav))
+// Listen for Nav click
+navItems.forEach(Item => Item.addEventListener("click", selectNav));
 
 eventClick.addEventListener("click", function() {
     eventRedirect.click()
-})
+});
 
 eventRedirect.addEventListener('click', function() {
     // does nothing
-})
+});
 
 eventFooterClick.addEventListener("click", function () {
     eventClick.click()
-})
+});
 
 contactClick.addEventListener("click", function () {
     contactRedirect.click()
-})
+});
 
 contactRedirect.addEventListener("click", function() {
     // does nothing
-})
+});
 
 contactFooterClick.addEventListener("click", function () {
     contactClick.click()
-})
+});
 
 aboutClick.addEventListener("click", function () {
     aboutRedirect.click()
-})
+});
 
 aboutRedirect.addEventListener("click", function() {
     // does nothing
-})
+});
 
 aboutFooterClick.addEventListener("click", function () {
     aboutClick.click()
-})
+});
 
 
 eventRequest.addEventListener('click', function() {
     eventResponse.click()
-})
+});
 
 eventResponse.addEventListener('click', function() {
     // empty
-})
+});
 
 contactRequest.addEventListener('click', function() {
     contactResponse.click()
-})
+});
 
 contactResponse.addEventListener('click', function() {
     // empty
-})
+});
 
 aboutRequest.addEventListener('click', function() {
     aboutResponse.click()
-})
+});
 
 aboutResponse.addEventListener('click', function() {
     // empty
-})
+});
+
+formBtn.addEventListener('click', renderForm);
 
 
 
 // Form-script
-
-const labels = document.querySelectorAll('.form-control label')
+const labels = document.querySelectorAll('.form-control label');
 
 
 
@@ -126,4 +144,4 @@ labels.forEach(label => {
         .split('')
         .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
         .join('')
-})
+});
